3beb08fdaf8660bf3f7e38b739224dd0cfc865cb
JW <jacekwoln@gmail.com>
2023-04-27 00:06:33 +0200

    Test

--- src/main/java/org/test/untitled1/WorldEditTest.java
+++ src/main/java/org/test/untitled1/WorldEditTest.java
@@ -16,20 +16,15 @@ import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.session.ClipboardHolder;
 import com.sk89q.worldedit.session.SessionManager;
+import com.sk89q.worldedit.world.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
-
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 public final class WorldEditTest extends JavaPlugin implements CommandExecutor {
-
 
     private WorldEditPlugin worldEdit;
 
@@ -49,18 +44,10 @@ public final class WorldEditTest extends JavaPlugin implements CommandExecutor {
             return true;
         }
         Player player = (Player) sender;
-        if (args.length < 3) {
-            player.sendMessage("Usage: /paste <schematicName> <x> <y> <z>");
-            return true;
-        }
-        String schematicName = args[0];
-        int x = Integer.parseInt(args[1]);
-        int y = Integer.parseInt(args[2]);
-        int z = Integer.parseInt(args[3]);
 
 
         //wczytywanie
-        String path = "C://schematic/test.schem";
+        String path = "D://MC/spigot_1.19.4/plugins/modernrestraunt1.schem";
         File file = new File(path);
         Clipboard clipboard;
         ClipboardFormat format = ClipboardFormats.findByFile(file);
@@ -74,18 +61,17 @@ public final class WorldEditTest extends JavaPlugin implements CommandExecutor {
 
 
         //Pokręcone zamienianie gracza Spigotowego na gracza WorldEditowego
-        com.sk89q.worldedit.entity.Player actor = BukkitAdapter.adapt(player); // WorldEdit's native Player class extends Actor
-        SessionManager manager = WorldEdit.getInstance().getSessionManager();
-        LocalSession localSession = manager.get(actor);
+        World actor = BukkitAdapter.adapt(player.getWorld()); // WorldEdit's native Player class extends Actor
 
         //wklejanie
-        try (EditSession editSession = WorldEdit.getInstance().newEditSession(localSession.getSelectionWorld())) {
+        try (EditSession editSession = WorldEdit.getInstance().newEditSession(actor)) {
             Operation operation = new ClipboardHolder(clipboard)
                     .createPaste(editSession)
-                    .to(BlockVector3.at(x, y, z))
-                    // configure here
+                    .to(BlockVector3.at(player.getLocation().getX(), player.getLocation().getY(),player.getLocation().getZ()))
+                    .ignoreAirBlocks(false)
                     .build();
             Operations.complete(operation);
+            editSession.close();
         } catch (WorldEditException e)
         {
             player.sendMessage("Nie wklejenie schematika "+e.getMessage());
