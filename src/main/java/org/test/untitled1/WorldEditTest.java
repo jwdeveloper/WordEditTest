18a14f3074707917194644dac6fc6eb3be9b9952
JW <jacekwoln@gmail.com>
2023-04-26 22:33:47 +0200

    Test

--- src/main/java/org/test/untitled1/WorldEditTest.java
+++ src/main/java/org/test/untitled1/WorldEditTest.java
@@ -1,17 +1,97 @@
 package org.test.untitled1;
 
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import com.sk89q.worldedit.extent.clipboard.io.BuiltInClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
+import com.sk89q.worldedit.function.operation.Operation;
+import com.sk89q.worldedit.function.operation.Operations;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.session.ClipboardHolder;
+import com.sk89q.worldedit.session.SessionManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
-public final class Untitled1dasadada extends JavaPlugin {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-    @Override
+public final class WorldEditTest extends JavaPlugin implements CommandExecutor {
+
+
+    private WorldEditPlugin worldEdit;
+
     public void onEnable() {
-        // Plugin startup logic
-
+        worldEdit = (WorldEditPlugin) getServer().getPluginManager().getPlugin("WorldEdit");
+        if (worldEdit == null) {
+            getLogger().warning("WorldEdit plugin not found! Disabling...");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        getCommand("paste").setExecutor(this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be used by players.");
+            return true;
+        }
+        Player player = (Player) sender;
+        if (args.length < 3) {
+            player.sendMessage("Usage: /paste <schematicName> <x> <y> <z>");
+            return true;
+        }
+        String schematicName = args[0];
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+        int z = Integer.parseInt(args[3]);
+
+
+        //wczytywanie
+        String path = "C://schematic/test.schem";
+        File file = new File(path);
+        Clipboard clipboard;
+        ClipboardFormat format = ClipboardFormats.findByFile(file);
+        try (ClipboardReader reader = format.getReader(new FileInputStream(file))) {
+            clipboard = reader.read();
+        } catch (Exception e)
+        {
+            player.sendMessage("Nie działa wczytanie schematika "+e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+
+        //Pokręcone zamienianie gracza Spigotowego na gracza WorldEditowego
+        com.sk89q.worldedit.entity.Player actor = BukkitAdapter.adapt(player); // WorldEdit's native Player class extends Actor
+        SessionManager manager = WorldEdit.getInstance().getSessionManager();
+        LocalSession localSession = manager.get(actor);
+
+        //wklejanie
+        try (EditSession editSession = WorldEdit.getInstance().newEditSession(localSession.getSelectionWorld())) {
+            Operation operation = new ClipboardHolder(clipboard)
+                    .createPaste(editSession)
+                    .to(BlockVector3.at(x, y, z))
+                    // configure here
+                    .build();
+            Operations.complete(operation);
+        } catch (WorldEditException e)
+        {
+            player.sendMessage("Nie wklejenie schematika "+e.getMessage());
+            throw new RuntimeException(e);
+        }
+        player.sendMessage("Schematic pasted successfully!");
+        return true;
     }
 }
